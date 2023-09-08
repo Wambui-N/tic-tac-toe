a100171f434e83986aa36c51902687bf09c456b4
Wambui Ndungu <wambuin18@gmail.com>
2023-09-08 14:49:27 +0300

    Added a function that find the best move for AI

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const player2 = "O";
 let currentPlayer = player1;
 const turn = document.getElementsByClassName("turn");
 turn[0].innerHTML = "Your turn"; //first player
-
+score = null;
 id = null;
 filled = [];
+score = 0;
 
 //turn taking
 const playerTurn = () => {
@@ -25,6 +26,9 @@ const winningConditions = [
   [2, 4, 6], //diagonals
 ];
 
+//score of different rows
+const rowScore = [];
+
 //clearing the board
 function clearCells() {
   //reset the board without reloading the page
@@ -34,6 +38,8 @@ function clearCells() {
     cell.classList.replace("bg-slate-900", "bg-slate-100");
     cell.classList.replace("text-slate-100", "text-slate-900");
     cell.addEventListener("click", cellClicked);
+    //empty the filled array
+    filled = [];
     // reset to player 1
     currentPlayer = player1;
     playerTurn();
@@ -58,9 +64,24 @@ const stopClicking = () => {
   });
 };
 
+//evaluate the board for the minimax algorithm
+function evaluateBoard(winner) {
+  // Check if the AI has won
+  if (winner === player2) {
+    score = 10;
+    return score;
+  }
+  // Check if the player has won
+  if (winner === player1) {
+    score = -10;
+    return score;
+  }
+  // It's a draw
+  return 0;
+}
+
 //checking for winner
 const checkWinner = (filled) => {
-  console.log(filled);
   let winner = null;
   for (let i = 0; i < winningConditions.length; i++) {
     const [a, b, c] = winningConditions[i];
@@ -80,15 +101,16 @@ const checkWinner = (filled) => {
         highlightCells(cell);
         stopClicking();
         winner = cell.a.innerHTML;
+        score = evaluateBoard(winner);
         const alert = document.getElementById("alert");
         alert.classList.replace("hidden", "visible");
         const alertText = document.getElementById("alert-text");
         alertText.innerHTML = `Player ${cell.a.innerHTML} won!`;
-        setTimeout(()=>{
+        setTimeout(() => {
           alert.classList.replace("visible", "hidden");
           alertText.innerHTML = "";
           clearCells();
-        }, 1000)
+        }, 1000);
       }
     }
     if (filled.length == 9 && winner == null) {
@@ -105,15 +127,9 @@ const checkWinner = (filled) => {
   }
 };
 
-//reload
-// const reload = () => {
-//   location.reload();
-// };
-
 //reset button
 const resetButton = document.getElementById("reset");
 resetButton.addEventListener("click", clearCells);
-
 
 //filling the board
 const cells = document.querySelectorAll(".cell");
@@ -141,4 +157,78 @@ function cellClicked(e) {
   }
   checkWinner(filled);
   playerTurn();
+}
+
+// function minimax(depth, isMaximizing) {
+//   if (score === 10 || score === -10 || filled.length == 9 ) {
+//     return score;
+//   }
+
+//   if (isMaximizing) {
+//     let bestScore = -Infinity;
+//     for (let i = 1; i < filled.length; i++) {
+//       if (currentPlayer === player1) {
+//         currentPlayer = player2;
+//         //filled[i] = player2;
+//         bestScore = Math.max(bestScore, minimax(filled, depth + 1, false));
+//         filled[i] = "";
+//       }
+//     }
+//     return bestScore;
+//   } else {
+//     let bestScore = Infinity;
+//     for (let i = 0; i < board.length; i++) {
+//       if (board[i] === "") {
+//         board[i] = player1;
+//         bestScore = Math.min(bestScore, minimax(board, depth + 1, true));
+//         board[i] = "";
+//       }
+//     }
+//     return bestScore;
+//   }
+// }
+
+//score the board
+// function scoreBoard() {
+//   for (let i = 0; i < filled.length; i++) {
+//     if (filled[i] === player1) {
+//       score -= 1;
+//     } else if (filled[i] === player2) {
+//       score += 1;
+//     }
+//   }
+//   return score;
+// }
+
+//player 2
+//best move
+function bestMove() {
+  let bestScore = -Infinity;
+  let bestMoveIndex = -1;
+
+  for (let i = 0; i < winningConditions.length; i++) {
+    if (filled.length < 3) {
+      break;
+    }
+    const [a, b, c] = winningConditions[i];
+    const cell = {
+      a: document.getElementById(a),
+      b: document.getElementById(b),
+      c: document.getElementById(c),
+    };
+    const cellsToCheck = [cell.a, cell.b, cell.c];
+    cellsToCheck.forEach((cellToCheck) => {
+      if (cellToCheck.innerHTML == player1) {
+        rowScore -= 1;
+      } else if (cellToCheck.innerHTML == player2) {
+        rowScore += 1;
+      }
+      // Calculate the rowScore based on the contents of the cells
+    });
+    if (rowScore > bestScore) {
+      bestScore = rowScore;
+      bestMoveIndex = i;
+    }
+    rowScore = 0;
+  }
 }
